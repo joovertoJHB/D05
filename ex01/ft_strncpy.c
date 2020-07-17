091ef33dbfb2500406d6a63217341ed4358aa1e1
joovertoJHB <jooverto@student.wethinkcode.co.za>
2020-07-17 11:23:42 +0200

    Added ex06

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: jooverto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/16 12:25:25 by jooverto          #+#    #+#             */
-/*   Updated: 2020/07/16 14:59:38 by jooverto         ###   ########.fr       */
+/*   Updated: 2020/07/17 11:23:29 by jooverto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,14 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	int		copy;
 
 	copy = 0;
-	while(copy <= n)
+	while(src[copy] != '\0' && copy < n)
 	{
-		dest[copy] = src[copy];
+		dest[i] = src [i];
+		copy++;
+	}
+	if(i < n)
+	{
+		dest[i] = '\0';
 		copy++;
 	}
 	return(dest);
